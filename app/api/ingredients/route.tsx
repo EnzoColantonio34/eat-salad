a7ed0bd2fb787e2e53cbe61a10210a142f6d476a
EnzoColantonio34 <enzo.colantonio@sfr.fr>
2025-06-05 12:13:00 +0200

    Add APIs (ingredients & order)

---
+++ app/api/ingredients/route.tsx
@@ -0,0 +1,52 @@
+// api/ingredients/route.tsx
+
+import type { NextRequest } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  {params} : {params: Promise<{slug: string}>} //facultatif
+) {
+
+  const searchParams = request.nextUrl.searchParams
+  const queryType = searchParams.get('type')
+  // on chercher à récupérer la valeur du paramètre dans l'URL
+
+  const veggies = [
+    "salad",
+    "olives",
+    "pickles"
+  ];
+
+  const proteins = [
+    "tuna",
+    "eggs",
+    "tofu"
+  ];
+
+  const sauces = [
+    "mustard",
+    "vinegar",
+    "lemon"
+  ];
+
+  let listIngredients = null;
+
+  switch(queryType) {
+    case "proteins":
+      listIngredients = proteins
+      break;
+    case "veggies":
+      listIngredients = veggies
+      break;
+    case "sauces":
+      listIngredients = sauces
+      break;    
+    default:
+    listIngredients = {veggies, proteins, sauces};
+  }
+
+  return new Response(JSON.stringify(listIngredients), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
---
+++ app/api/order/route.tsx
@@ -0,0 +1,15 @@
+import type { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  // Parse the request body
+  const body = await request.json();
+  const { username, email, ingredientsList } = body;
+ 
+  // e.g. Insert new user into your DB
+  const newOrder = { id: Date.now(), username, email, ingredientsList };
+ 
+  return new Response(JSON.stringify(newOrder), {
+    status: 201,
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
